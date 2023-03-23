538655945831833ef76c9ef583fdad69d76347e2
Wlisk <richard.mzrosa@gmail.com>
2023-03-23 12:55:51 -0300

    tests in the write_binary.py - convertion tests

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,1 +1,17 @@
+from scripts.entity.entity_info import BaseTuple
+
 NOT_FOUND = -1
+
+# COMPARE FUNCTIONS
+def search_by_id(e: BaseTuple, v: int): return e.id == v
+def search_by_name(e: BaseTuple, v: str): return e.name == v
+
+# converts a string of bytes into python string
+def bytes_to_str(data: bytes) -> str:
+    # split at the first '\0' 
+    # and return the first splited part converted as string
+    return str( data.split(b'\0', 1)[0], 'utf-8' )
+
+# converts a string to a string of bytes
+def str_to_bytes(data: str) -> bytes:
+    return bytes(data, 'utf-8')
--- tests/write_binary.py
+++ tests/write_binary.py
@@ -12,19 +12,27 @@ import struct
 from scripts.entity.entity_info \
     import EntityInfo, ENTITY, HEADER, EntityTuple, BaseTuple
 from scripts.entity.io import \
-    read_header, read_data, read_data_at, \
-    search_data, search_by_id
+    read_header, read_data, read_data_at, read_data_from_index, \
+    search_data
+
+from scripts.utils import bytes_to_str, str_to_bytes
 
 f = open('genres.test.bin', 'rb')
 
-print(read_header(f))
+#print(read_header(f))
 
 def update_header(entity_file: BufferedReader, entity: EntityInfo):
     entity_file.seek(0)
 
-    entity_file.write()
+    #entity_file.write()
+    return
 
+id, name = read_data_from_index(f, ENTITY['genres'], 1)
+name = bytes_to_str(name)
+print(id, name)
 
+u = struct.pack(ENTITY['genres'].struct_format, id, str_to_bytes(name))
+print(u)
 
 
 f.close()
